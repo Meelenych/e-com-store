216fb43d50e15b3fb0f44b21b85689e286ef28ec
Meelenych <meelenych@gmail.com>
2024-01-24 13:33:09 -0700

    optimized production buid

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-// Import required modules
 const express = require('express');
 const mongoose = require('mongoose');
 
-// Create an Express application
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect to MongoDB (replace connection string and database name)
-mongoose.connect(
-	'mongodb+srv://meelenych:Trsd1583-@e-commerce-store.jmz1jj6.mongodb.net/e-store',
-);
+// Connect to MongoDB using the environment variable set on Heroku
+mongoose.connect(process.env.MONGODB_URI);
 
-// Get the default connection
 const db = mongoose.connection;
-
-// Bind connection to error event (to get notified of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// Bind connection to open event (to get notified when the connection is open)
 db.once('open', () => {
 	console.log('Connected to MongoDB');
 });
 
-// Define a Product schema
 const productSchema = new mongoose.Schema({
 	_id: String,
 	name: String,
@@ -31,41 +21,11 @@ const productSchema = new mongoose.Schema({
 	imagePath: String,
 });
 
-// Create a Product model based on the schema
 const Product = mongoose.model('Product', productSchema, 'products');
 
-// Insert dummy data into the products collection
-Product.insertMany([
-	{
-		name: 'Tomatoes',
-		price: '5.05',
-		quantity: '100',
-		imagePath:
-			'https://cdn.pixabay.com/photo/2016/03/26/16/44/tomatoes-1280859_1280.jpg',
-	},
-	{
-		name: 'Potatoes',
-		price: '5.05',
-		quantity: '100',
-		imagePath:
-			'https://cdn.pixabay.com/photo/2016/08/11/08/49/potatoes-1585075_1280.jpg',
-	},
-	{
-		name: 'Onions',
-		price: '5.05',
-		quantity: '100',
-		imagePath:
-			'https://cdn.pixabay.com/photo/2017/06/15/09/20/onion-2404583_1280.jpg',
-	},
-]);
-
-// Endpoint to get all products
 app.get('/api/e-store/products', async (req, res) => {
 	try {
-		// Set cache control header to disable caching
 		res.set('Cache-Control', 'no-store');
-
-		// Retrieve all products from the database
 		const products = await Product.find();
 		console.log(products);
 		res.json(products);
@@ -75,7 +35,6 @@ app.get('/api/e-store/products', async (req, res) => {
 	}
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
